5f0fbb8486712419354aec9819af21b85c2db33e
Bdennis88 <70287873+Bdennis88@users.noreply.github.com>
2020-08-31 14:26:55 -0500

    Add files via upload

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public object LabelMessage { get; private set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Hello World";
+            Message_.Content = message;
+
+
+
+        
+
+        }
+    }
+}
