08104e269e976d1c36c37a555963a6ef05a4bbcb
Bdennis88 <70287873+Bdennis88@users.noreply.github.com>
2020-09-01 13:48:55 -0500

    Add files via upload

---
+++ FunWithVariables/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace FunWithVariables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMyMessage_Click(object sender, RoutedEventArgs e)
+        {
+
+            string firstName;
+
+            string lastName;
+
+            firstName = textBoxFirstName.Text;
+
+            lastName = textBoxLastName.Text;
+
+            labelMyMessage.Content = "Hello " + firstName + " " + lastName + ", good to see you! ";
+
+
+
+        }
+
+        private void buttonClearMe_Click(object sender, RoutedEventArgs e)
+        {
+
+            textBoxLastName.Text = "";
+
+            textBoxFirstName.Text = "";
+
+            labelMyMessage.Content = "";
+
+
+        }
+    }
+}
